6ac4c4d59b59070be92b97438cadfd864f31e20f
pretty <734396695@qq.com>
2018-11-08 20:59:30 +0800

    bdb,小型数据库，阻塞队列

---
+++ docservicedeamon/src/main/java/com/pretty/doccloud/demo/PersistenceQueueDemo.java
@@ -0,0 +1,27 @@
+package com.pretty.doccloud.demo;
+
+import com.pretty.doccloud.util.BdbPersistentQueue;
+
+/*
+*@ClassName:PersistenceQueueDemo
+ @Description:TODO
+ @Author:
+ @Date:2018/10/30 15:49 
+ @Version:v1.0
+*/
+public class PersistenceQueueDemo {
+    public static void main(String[] args) {
+        BdbPersistentQueue<String> queue = new BdbPersistentQueue<String>("d:\\bdb", "test", String.class);
+
+       /*queue.offer("tri");
+        queue.offer("qua");*/
+       String data1 = queue.poll();
+        //String data2 = queue.poll();
+
+        System.out.println(data1);
+        //System.out.println(data2);
+
+    }
+
+
+}
